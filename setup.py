e00339c0cf8e4eaddf3e5afef721cd9a13bb02af
ZikangXiong <zikangxiong@icloud.com>
2021-05-10 22:04:13 -0400

    small fix

--- setup.py
+++ setup.py
@@ -12,12 +12,12 @@ setup(
     packages=['safe_rl'],
     install_requires=[
         'gym>=0.15.3',
-        'joblib==0.14.0',
-        'matplotlib==3.1.1',
+        'joblib>=0.14.0',
+        'matplotlib>=3.1.1',
         'mpi4py==3.0.2',
         'mujoco_py==2.0.2.7',
-        'numpy~=1.17.4',
-        'seaborn==0.8.1',
-        'tensorflow==1.13.1',
+        'numpy>=1.17.4',
+        'seaborn>=0.8.1',
+        'tensorflow==1.14.1',
     ],
 )
